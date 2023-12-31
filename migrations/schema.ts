a5474782d7e983d0c365bf9781db252560f64271
matthewraymondh <raymondhartono76@gmail.com>
2023-12-31 16:36:00 +0700

    database refactor

--- migrations/schema.ts
+++ migrations/schema.ts
@@ -72,6 +72,16 @@ export const subscriptionStatus = pgEnum("subscription_status", [
   "unpaid",
 ]);
 
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().notNull(),
+  fullName: text("full_name"),
+  avatarUrl: text("avatar_url"),
+  billingAddress: jsonb("billing_address"),
+  updatedAt: timestamp("updated_at", { withTimezone: true, mode: "string" }),
+  paymentMethod: jsonb("payment_method"),
+  email: text("email"),
+});
+
 export const customers = pgTable("customers", {
   id: uuid("id").primaryKey().notNull(),
   stripeCustomerId: text("stripe_customer_id"),
